197798d6d7d4056ab4487f6ab95c38392afebc11
Ilya Krasnov <ilya.krasnov@gmail.com>
2016-03-29 09:07:52 +0200

    Adds seeds rb and events show

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,12 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 user = CreateAdminService.new.call
 puts 'CREATED ADMIN USER: ' << user.email
+
+event = Event.create(name: 'Shabbat in Jerusalem')
+meal1 = Meal.create(name: 'Evening', event: event)
+meal2 = Meal.create(name: 'Lunch', event: event)
+item1 = Item.create(name: 'Wine', meal: meal1)
+item2 = Item.create(name: 'Cake', meal: meal1)
+item3 = Item.create(name: 'Shnitzels', meal: meal1)
+item4 = Item.create(name: 'Coke', meal: meal2)
+item5 = Item.create(name: 'Juice', meal: meal2)
